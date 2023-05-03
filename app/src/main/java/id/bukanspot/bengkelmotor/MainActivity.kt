b946f8af89535feb7c3e801d2bcadb8185c8114f
Indra Wahyu <bukanspot@gmail.com>
2023-05-03 22:39:02 +0800

    create project

---
+++ app/src/main/java/id/bukanspot/bengkelmotor/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.bukanspot.bengkelmotor
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
