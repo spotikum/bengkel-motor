67a4b67cca6c44784211b39b01c61294d467259c
Indra Wahyu <bukanspot@gmail.com>
2023-05-09 14:54:36 +0800

    add buildFeatures

--- app/src/main/java/id/bukanspot/bengkelmotor/MainActivity.kt
+++ app/src/main/java/id/bukanspot/bengkelmotor/MainActivity.kt
@@ -1,6 +1,7 @@
 package id.bukanspot.bengkelmotor
 
-import android.support.v7.app.AppCompatActivity
+//import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
