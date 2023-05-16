8b817b919769f709e1e1224e0b1d4a3cdde4e3e2
Indra Wahyu <bukanspot@gmail.com>
2023-05-16 22:43:44 +0800

    add notification button

--- app/src/main/java/id/bukanspot/bengkelmotor/HomeActivity.kt
+++ app/src/main/java/id/bukanspot/bengkelmotor/HomeActivity.kt
@@ -1,12 +1,16 @@
 package id.bukanspot.bengkelmotor
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.material.badge.BadgeUtils
 import id.bukanspot.bengkelmotor.adapter.ViewPagerAdapter
 import id.bukanspot.bengkelmotor.databinding.ActivityHomeBinding
 import id.bukanspot.bengkelmotor.databinding.ActivityMainBinding
@@ -38,5 +42,25 @@ class HomeActivity : AppCompatActivity() {
 
         binding.tabs.getTabAt(0)!!.setIcon(R.drawable.ic_home)
         binding.tabs.getTabAt(1)!!.setIcon(R.drawable.ic_user)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+
+        // Get the notification icon view
+//        val notification = menu.findItem(R.id.notification)
+//        val icon = notification.icon
+
+//        // Set the badge count
+//        BadgeUtils.setBadgeCount(this, notification, icon, 2)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.notification -> Toast.makeText(this, "Notification", Toast.LENGTH_LONG).show()
+        }
+        return true
     }
 }
