60e2160831f1ce6f77f6ca21ef020f1a60f08956
Indra Wahyu <bukanspot@gmail.com>
2023-05-17 10:50:49 +0800

    remove booking test

--- app/src/main/java/id/bukanspot/bengkelmotor/AddBookingActivity.kt
+++ app/src/main/java/id/bukanspot/bengkelmotor/AddBookingActivity.kt
@@ -1,8 +1,12 @@
 package id.bukanspot.bengkelmotor
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
 
 class AddBookingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +15,18 @@ class AddBookingActivity : AppCompatActivity() {
 
         val actionBar = supportActionBar
         actionBar?.setDisplayHomeAsUpEnabled(true)
+
+//        val database = Firebase.database
+//        val myRef = database.getReference("message")
+//
+//        myRef.setValue("Hello, World!")
+
+//        val ref = Firebase.database("https://bengkel-motor-cea56-default-rtdb.asia-southeast1.firebasedatabase.app").reference
+
+        val database = FirebaseDatabase.getInstance("https://bengkel-motor-cea56-default-rtdb.asia-southeast1.firebasedatabase.app")
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
