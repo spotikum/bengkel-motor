cdfec4a7149475f1f6ac46a04bc0447d13aeefb1
Indra Wahyu <wicaksana0701@gmail.com>
2023-07-06 03:09:37 +0800

    insert book to database

---
+++ app/src/main/java/id/bukanspot/bengkelmotor/BookingModel.kt
@@ -0,0 +1,8 @@
+package id.bukanspot.bengkelmotor
+
+data class BookingModel (
+    var Id: String? = null,
+    var Name: String? = null,
+    var Plat: String? = null,
+    var Keluhan: String? = null
+)
